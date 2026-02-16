83105b569da752b9d106faebd681caf9d5f751f0
Noman <pxycknomdictator@outlook.com>
2026-02-16 14:47:42 +0500

    feat: add project types in codebase

---
+++ src/types/index.ts
@@ -0,0 +1,24 @@
+export type SQL_DATABASE = "mysql" | "mariadb" | "postgres";
+export type SQL_ORMS = "prisma" | "drizzle" | "typeorm" | "sequelize";
+
+export type NO_SQL_DATABASE = "mongodb";
+export type NO_SQL_ORMS = "prisma" | "typeorm" | "mongoose";
+
+export type SQL_DB_CONFIG = {
+    name: SQL_DATABASE;
+    orms: SQL_ORMS;
+};
+
+export type NO_SQL_DB_CONFIG = {
+    name: NO_SQL_DATABASE;
+    orms: NO_SQL_ORMS;
+};
+
+export type Config = {
+    language: "ts" | "js";
+    packageManager: "npm" | "yarn" | "pnpm" | "bun";
+    database: {
+        sql?: SQL_DB_CONFIG[];
+        no_sql?: NO_SQL_DB_CONFIG[];
+    };
+};
