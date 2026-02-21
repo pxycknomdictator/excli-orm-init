c0c4af50a9b04162b6dcf5251928b046175fc871
Noman <pxycknomdictator@outlook.com>
2026-02-22 04:17:54 +0500

    feat: add utility fn to generate file with content

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,11 @@
 import { cancel } from "@clack/prompts";
+import { writeFile } from "node:fs/promises";
+import type { GenerateFileArgs } from "src/types";
+
+export async function generateFile(fileArgs: GenerateFileArgs) {
+    const { fileLocation, fileContent } = fileArgs;
+    await writeFile(fileLocation, fileContent, "utf-8");
+}
 
 export function terminate(message: string): never {
     cancel(message);
