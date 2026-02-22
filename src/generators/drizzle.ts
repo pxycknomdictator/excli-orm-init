57ab19d08bdf04ef81d5c5a2ddf31a70d76624f5
Noman <pxycknomdictator@outlook.com>
2026-02-22 20:38:59 +0500

    feat: add complete drizzle orm dbms configurations

--- src/generators/drizzle.ts
+++ src/generators/drizzle.ts
@@ -5,7 +5,12 @@ import {
     schemasLocation,
 } from "src/config";
 import { concatFileExtension, generateFile } from "src/utils";
-import type { GenerateFileArgs, ProjectConfig, SQL_DATABASE } from "src/types";
+import type {
+    GenerateFileArgs,
+    Language,
+    ProjectConfig,
+    SQL_DATABASE,
+} from "src/types";
 
 export async function setupDrizzle({ language, database }: ProjectConfig) {
     const [drizzlePath, dbPath, schemasPath] = concatFileExtension(
@@ -15,13 +20,21 @@ export async function setupDrizzle({ language, database }: ProjectConfig) {
         schemasLocation,
     );
 
+    const drizzleSchema =
+        database === "postgres"
+            ? drizzlePostgresSchema
+            : drizzleMysqlMariadbSchema;
+
     const drizzle: GenerateFileArgs[] = [
         {
             fileLocation: drizzlePath!,
             fileContent: drizzleConfigContent(database as SQL_DATABASE),
         },
-        { fileLocation: dbPath!, fileContent: "db config" },
-        { fileLocation: schemasPath!, fileContent: "schemas config" },
+        {
+            fileLocation: dbPath!,
+            fileContent: drizzleConnection(database as SQL_DATABASE, language),
+        },
+        { fileLocation: schemasPath!, fileContent: drizzleSchema() },
     ];
 
     await Promise.all(
@@ -42,5 +55,61 @@ export default defineConfig({
         url: process.env.DATABASE_URL!,
     },
 });
+`;
+}
+
+function drizzlePostgresSchema() {
+    return `import {
+    uuid,
+    integer,
+    pgTable,
+    varchar,
+    boolean,
+    timestamp,
+} from "drizzle-orm/pg-core";
+
+export const usersTable = pgTable("users", {
+    id: uuid("id").primaryKey().defaultRandom(),
+    name: varchar("name", { length: 255 }).notNull(),
+    age: integer("age").notNull(),
+    email: varchar("email", { length: 255 }).notNull().unique(),
+    isActive: boolean("is_active").default(true),
+    createdAt: timestamp("created_at", { withTimezone: true }).defaultNow(),
+    updatedAt: timestamp("updated_at", { withTimezone: true })
+        .defaultNow()
+        .$onUpdate(() => new Date()),
+});  
+`;
+}
+
+function drizzleMysqlMariadbSchema() {
+    return `import {
+    mysqlTable,
+    varchar,
+    int,
+    boolean,
+    timestamp,
+} from "drizzle-orm/mysql-core";
+
+export const usersTable = mysqlTable("users", {
+    id: int("id").primaryKey().autoincrement(),
+    name: varchar("name", { length: 255 }).notNull(),
+    age: int("age").notNull(),
+    email: varchar("email", { length: 255 }).notNull(),
+    isActive: boolean("isActive").default(true),
+    createdAt: timestamp("created_at", { mode: "date" }).defaultNow(),
+    updatedAt: timestamp("updated_at", { mode: "date" })
+        .defaultNow()
+        .$onUpdate(() => new Date()),
+});
+`;
+}
+
+function drizzleConnection(db: SQL_DATABASE, lang: Language) {
+    const module = db === "postgres" ? "node-postgres" : "mysql2";
+
+    return `import { drizzle } from "drizzle-orm/${module}";
+
+export const db = drizzle(process.env.DATABASE_URL${lang === "ts" ? "!" : ""});
 `;
 }
