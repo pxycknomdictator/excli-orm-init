373254b2bb17c0cb624f0b2e8a01589431417ed8
Noman <pxycknomdictator@outlook.com>
2026-02-26 14:23:33 +0500

    feat: add sql and nosql prisma connection

--- src/generators/prisma.ts
+++ src/generators/prisma.ts
@@ -1,21 +1,52 @@
-import { prismaDialectMap, prismaSchemaLocation } from "src/config";
-import { appendExistsFile, fireShell } from "src/utils";
-import type { ProjectConfig } from "src/types";
+import { dbLocation, prismaDialectMap, prismaSchemaLocation } from "src/config";
+import {
+    appendExistsFile,
+    concatFileExtension,
+    fireShell,
+    generateFile,
+} from "src/utils";
+import type { ProjectConfig, GenerateFileArgs } from "src/types";
+
+const prismaConnectionDatabasesList: Record<
+    ProjectConfig["database"],
+    (db: ProjectConfig["database"]) => string
+> = {
+    mysql: prismaMysqlMariadbConnection,
+    mariadb: prismaMysqlMariadbConnection,
+    postgres: prismaPostgresConnection,
+    mongodb: prismaMongodbConnection,
+};
 
 export async function setupPrisma(config: ProjectConfig) {
-    const { databaseType } = config;
+    const { databaseType, database, language } = config;
 
     const prismaSchema =
         databaseType === "sql" ? prismaSqlSchema : prismaNoSqlSchema;
 
-    await initializePrisma(config.database);
+    await initializePrisma(database);
     await appendExistsFile({
         fileLocation: prismaSchemaLocation,
         fileContent: prismaSchema(),
     });
+
+    const [dbPath] = concatFileExtension(language, dbLocation);
+    const prismaConnectionGenerator = prismaConnectionDatabasesList[database];
+
+    const prisma: GenerateFileArgs[] = [
+        {
+            fileLocation: dbPath!,
+            fileContent: prismaConnectionGenerator(database),
+        },
+    ];
+
+    await Promise.all(
+        prisma.map(async (config) => await generateFile({ ...config })),
+    );
+
+    await fireShell("npx prisma generate");
 }
 
-export function prismaSqlSchema() {
+function prismaSqlSchema() {
     return `model User {
   id        Int      @id @default(autoincrement())
   name      String   @db.VarChar(255)
@@ -30,7 +61,7 @@ export function prismaSqlSchema() {
 `;
 }
 
-export function prismaNoSqlSchema() {
+function prismaNoSqlSchema() {
     return `model User {
   id        String @id @default(auto()) @map("_id") @db.ObjectId
   name      String
@@ -45,12 +76,50 @@ export function prismaNoSqlSchema() {
 `;
 }
 
-export async function initializePrisma(db: ProjectConfig["database"]) {
+async function initializePrisma(db: ProjectConfig["database"]) {
     try {
-        const database = db !== "mariadb" ? prismaDialectMap[db] : "mysql";
-        const prismaCli = `npx prisma init --datasource-provider ${database} --output ../src/generated/prisma`;
+        const provider = db !== "mariadb" ? prismaDialectMap[db] : "mysql";
+        const prismaCli = `npx prisma init --datasource-provider ${provider} --output ../src/generated/prisma`;
         await fireShell(prismaCli);
-    } catch (error) {
-        throw new Error("failed to generate prisma config");
+    } catch {
+        throw new Error("Failed to generate Prisma config");
     }
+}
+
+function prismaMysqlMariadbConnection(db: ProjectConfig["database"]) {
+    const prefix = db === "mysql" ? "MYSQL_" : "MARIADB_";
+
+    return `
+import { PrismaMariaDb } from "@prisma/adapter-mariadb";
+import { PrismaClient } from "../generated/prisma/client.js";
+
+const adapter = new PrismaMariaDb({
+  host: process.env.${prefix}HOST,
+  user: process.env.${prefix}USER,
+  password: process.env.${prefix}PASSWORD,
+  database: process.env.${prefix}DATABASE,
+  connectionLimit: 5,
+});
+
+export const prisma = new PrismaClient({ adapter });
+`;
+}
+
+function prismaPostgresConnection(_db: ProjectConfig["database"]) {
+    return `
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "../generated/prisma/client.js";
+
+const connectionString = process.env.DATABASE_URL!;
+
+const adapter = new PrismaPg({ connectionString });
+export const prisma = new PrismaClient({ adapter });
+`;
+}
+
+function prismaMongodbConnection(_db: ProjectConfig["database"]) {
+    return `
+import { PrismaClient } from "../generated/prisma/client.js";
+export const prisma = new PrismaClient();
+`;
 }
