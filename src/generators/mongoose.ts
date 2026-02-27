16e86c0cde019286f2dde37c1824b76d4077a271
Noman <pxycknomdictator@outlook.com>
2026-02-27 19:36:39 +0500

    feat(mongoose): add complete mongoose odm configurations in cli

--- src/generators/mongoose.ts
+++ src/generators/mongoose.ts
@@ -1,3 +1,55 @@
-import type { ProjectConfig } from "src/types";
+import { dbLocation, schemasLocation } from "src/config";
+import { concatFileExtension, generateFile } from "src/utils";
+import type { GenerateFileArgs, ProjectConfig } from "src/types";
 
-export async function setupMongoose(_config: ProjectConfig) {}
+export async function setupMongoose(config: ProjectConfig) {
+    const { language } = config;
+
+    const [dbPath, schemasPath] = concatFileExtension(
+        language,
+        dbLocation,
+        schemasLocation,
+    );
+
+    const mongoose: GenerateFileArgs[] = [
+        { fileLocation: dbPath!, fileContent: mongooseConnection(config) },
+        { fileLocation: schemasPath!, fileContent: mongooseSchema() },
+    ];
+
+    await Promise.all(
+        mongoose.map(async (config) => await generateFile({ ...config })),
+    );
+}
+
+function mongooseConnection(config: ProjectConfig) {
+    const { language } = config;
+
+    return `import mongoose from "mongoose";
+
+export async function database() {
+    try {
+        await mongoose.connect(process.env.DATABASE_URL${language === "ts" ? "!" : ""});
+        console.log("Database is connected!");
+    } catch (error) {
+        throw new Error("failed to connect with database: ", { cause: error });
+    }
+}
+`;
+}
+
+function mongooseSchema() {
+    return `import { Schema, model } from "mongoose";
+
+const userSchema = new Schema(
+    {
+        name: { type: String, required: true },
+        age: { type: Number, required: true },
+        email: { type: String, unique: true, required: true },
+        isActive: { type: Boolean, default: true },
+    },
+    { timestamps: true },
+);
+
+export const User = model("User", userSchema);
+`;
+}
