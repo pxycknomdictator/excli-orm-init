1d0f8b89b3648bd70117ca29b49e7df525a32067
Noman <pxycknomdictator@outlook.com>
2026-03-01 00:17:10 +0500

    feat(sequelize): add sequelize schemas in cli

--- src/generators/sequelize.ts
+++ src/generators/sequelize.ts
@@ -1,3 +1,43 @@
 import type { ProjectConfig } from "src/types";
 
-export async function setupSequelize(_config: ProjectConfig) {}
+export async function setupSequelize(_config: ProjectConfig) {
+    sequelizeSchema();
+}
+
+function sequelizeSchema() {
+    return `import { DataTypes } from "sequelize";
+import { sequelize } from "../index.js";
+
+export const User = sequelize.define(
+    "User",
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        age: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+        },
+        isActive: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: true,
+        },
+    },
+    {
+        tableName: "users",
+        timestamps: true,
+    },
+);
+`;
+}
